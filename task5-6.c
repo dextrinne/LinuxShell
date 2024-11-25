62e7637b7ccfd245855216619bbe540269eca718
dextrinne <167472397+dextrinne@users.noreply.github.com>
2024-11-25 11:59:30 +0300

    Add files via upload

---
+++ task5-6.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 1024
+
+// Функция для обработки команды echo
+bool handle_echo(char *command, char *output) {
+    if (strncmp(command, "echo ", 5) == 0) {
+        strcpy(output, command + 5); // Копируем текст после "echo"
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    
+    // Задание 5-6: команда echo и проверка этой команды
+    char buffer[BUFFER_SIZE]; // Ввод команды
+    char output[BUFFER_SIZE]; // Вывод строки
+
+    while (1) {
+        printf("Введите команду echo и текст после команды (если хотите выйти введите exit):\n");
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+            break; // Выход при Ctrl+D
+        }
+
+        buffer[strcspn(buffer, "\n")] = 0; // Удаляем символ новой строки
+
+        if (strcmp(buffer, "exit") == 0) {
+            break; // Выход при вводе "exit"
+        }
+
+        if (handle_echo(buffer, output)) {
+            printf("%s\n", output);
+        } 
+        
+        else {
+             //Здесь можно добавить обработку других команд или вывод сообщения об ошибке
+            printf("Неправильный ввод: %s\n", buffer);
+        }
+        printf("\n");
+    }
+    printf("Выход из программы.\n");
+    return 0;
+}
+
+
