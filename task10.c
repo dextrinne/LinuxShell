515d26d64e5cfac71d39fe5ad2dc6e9e7b0955c3
dextrinne <167472397+dextrinne@users.noreply.github.com>
2024-11-29 22:28:26 +0300

    Update task10.c

--- task10.c
+++ task10.c
@@ -3,18 +3,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h> 
 
 int main(int argc, char *argv[]) {
     printf("Задание 10: определить является ли диск загрузочным\n");
     printf("\n");
-    
-    if (argc != 2) {
-        fprintf(stderr, "Использование: %s <диск>\n", argv[0]);
+
+    char device_name[100];
+
+    printf("Введите имя устройства (например, sda, sdb): ");
+    if (fgets(device_name, sizeof(device_name), stdin) == NULL) {
+        fprintf(stderr, "Ошибка чтения с клавиатуры.\n");
         return 1;
     }
 
+    // Удаляем перенос строки, добавленный fgets
+    device_name[strcspn(device_name, "\n")] = 0;
+
     char path[20];
-    snprintf(path, sizeof(path), "/dev/%s", argv[1]);
+    snprintf(path, sizeof(path), "/dev/%s", device_name);
 
     int fd = open(path, O_RDONLY);
     if (fd < 0) {
@@ -32,10 +39,11 @@ int main(int argc, char *argv[]) {
     close(fd);
 
     if (buffer[510] == 0x55 && buffer[511] == 0xAA) {
-        printf("Диск %s является загрузочным.\n", argv[1]);
+        printf("Диск %s является загрузочным.\n", device_name);
     } else {
-        printf("Диск %s не является загрузочным.\n", argv[1]);
+        printf("Диск %s не является загрузочным.\n", device_name);
     }
 
     return 0;
 }
+
