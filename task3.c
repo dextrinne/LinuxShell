c6cfb244298fe13b84a2a942605cffe6682e75d6
dextrinne <167472397+dextrinne@users.noreply.github.com>
2024-12-06 17:41:42 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -9,7 +9,7 @@ int main() {
   //Задание 3  
   printf("Задание 3: Добавление команд выхода\n");
   char input[BUFFER_SIZE];
-  printf("Введите команду для закрытия (exit или \q)\n");
+  printf("Введите команду для закрытия (exit или q)\n");
   fgets(input, BUFFER_SIZE, stdin);
   input[strcspn(input, "\n")] = 0; // Удаляем символ новой строки
   if (strcmp(input, "exit") == 0 || strcmp(input, "\\q") == 0) {
